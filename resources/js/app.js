a0316ed387d9ad0d1871e120413d0726481513b5
Luiz Gustavo Martins <lgustavoms@gmail.com>
2025-04-16 14:41:17 -0300

    chore(build): update frontend config and setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,19 @@
 import './bootstrap';
+import '../css/app.css';
+
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { createPinia } from 'pinia';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+
+createInertiaApp({
+    resolve: name => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    setup({ el, App, props, plugin }) {
+        const app = createApp({ render: () => h(App, props) });
+        const pinia = createPinia();
+        
+        app.use(plugin)
+           .use(pinia)
+           .mount(el);
+    },
+});
